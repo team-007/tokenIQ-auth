3a10b60d7c36e1b02ef1b57a1e7be4732072fdf0
weirdestnerd <olu1995@hotmail.com>
2019-03-27 12:58:16 -0700

    allow user to input username and password, authenticate inputs with tokenIQ

--- app/login/login-view-model.js
+++ app/login/login-view-model.js
@@ -1,10 +1,23 @@
 const observableModule = require("data/observable");
 const utilityModule = require('tns-core-modules/utils/utils');
+const http = require('tns-core-modules/http');
 
 function LoginViewModel() {
     const viewModel = observableModule.fromObject({
         login() {
-            //  TODO: call WordPress log in here, then open ./home/home-page
+            http.request({
+                url: "tokeniq.io/wp-json/aam/v1/authenticate",
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                content: JSON.stringify({
+                    username: viewModel.get('username'),
+                    password: viewModel.get('password')
+                })
+            }).then((response) => {
+                //TODO: if success, response.content is JSON, else is string
+                const result = response.content.toJSON();
+            }, (e) => {
+            });
         },
         signup() {
             utilityModule.openUrl('https://wordpress.com/start/user?ref=homepage');
