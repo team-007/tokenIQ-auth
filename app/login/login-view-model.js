99e028ac34fcb33d3509b5a343298ae52ab1ffaa
skailas <smitha.gopalan26@gmail.com>
2019-04-22 20:12:30 -0700

    Integrated auth and firebase code

--- app/login/login-view-model.js
+++ app/login/login-view-model.js
@@ -1,13 +1,19 @@
 const observableModule = require("tns-core-modules/data/observable");
 const utilityModule = require('tns-core-modules/utils/utils');
 const http = require('tns-core-modules/http');
+const dialogs = require("tns-core-modules/ui/dialogs");
+const ActivityIndicator = require("tns-core-modules/ui/activity-indicator").ActivityIndicator;
+const frameModule = require("tns-core-modules/ui/frame");
 const topmost = require('tns-core-modules/ui/frame').topmost;
 
 function LoginViewModel() {
+    isLoading = false;
     const viewModel = observableModule.fromObject({
+        isLoading,
         login() {
+            viewModel.isLoading = true;
             http.request({
-                url: "https://tokeniq.herokuapp.com/authenticate",
+                url: "https://interns-tiq-invest-sandboxes.pantheonsite.io/wp-json/aam/v1/authenticate",
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 content: JSON.stringify({
@@ -15,15 +21,27 @@ function LoginViewModel() {
                     password: viewModel.get('password')
                 })
             }).then((response) => {
-                const result = response.content.toJSON();
-                topmost().navigate({
-                    moduleName: 'push-notification/push-notification-page',
-                    clearHistory: true,
-                    context: {
-                        username: result.username,
-                        usertoken: result.token
-                    }
-                })
+                //const result = response.content.toJSON();
+                var resp_str = String(response.content);
+                console.log(resp_str);
+
+                viewModel.isLoading = false;
+
+                if(resp_str.substring(1, 9) == "<strong>" || resp_str == '""') {
+                  //console.log("ree");
+                  dialogs.alert("Invalid username or password");
+                }
+                else {
+                  console.log(response.content);
+                  topmost().navigate({
+                      moduleName: 'push-notification/push-notification-page',
+                      clearHistory: true,
+                      context: {
+                          username: result.username,
+                          usertoken: result.token
+                      }
+                  });
+                }
             }, (e) => {
                 console.error(e);
             });
